31c06b4fa1de53a2019f1da3466d05b5237e1a4a
jianjianzhu <1611403@mail.nankai.edu.cn>
2021-10-10 13:13:37 +0800

    Initial Commit

---
+++ LaughterDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaughterDetector
+//
+//  Created by 朱彦谕 on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
