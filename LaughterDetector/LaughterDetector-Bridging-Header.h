171a26f41cbfc9335feebc76da340bf909d7c3ed
jianjianzhu <jianjianzhu@tencent.com>
2022-06-19 18:22:28 +0800

    first commit

---
+++ LaughterDetector/LaughterDetector-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  LaughterDetector-Bridging-Header.h
+//  LaughterDetector
+//
+//  Created by 朱彦谕 on 2021/10/10.
+//
+
+#ifndef LaughterDetector_Bridging_Header_h
+#define LaughterDetector_Bridging_Header_h
+
+
+#endif /* LaughterDetector_Bridging_Header_h */
